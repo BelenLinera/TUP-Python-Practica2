da94f85ddf9f873736921471756293e18ecd4a96
Belen Linera <belen.linera@gmail.com>
2022-08-31 12:47:10 -0300

    Ejer 5 falta

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -1,20 +1,15 @@
 """For, Sum, Reduce."""
 
-
-def sumatoria_basico(n: int) -> int:
-    suma = 0
-    for i in range(1, n):
-        suma = suma + i
-    return suma
-
-
-n = int(input("Ingresar un número: "))
-print(sumatoria_basico(n))
-
-
 """Devuelve la suma de los números de 1 a N.
     Restricción: Utilizar un bucle FOR.
     """
+
+def sumatoria_basico(n: int) -> int:
+    suma=0
+    for i in range(n):
+        suma = suma + (i+1)
+    return suma
+
 # NO MODIFICAR - INICIO
 assert sumatoria_basico(1) == 1
 assert sumatoria_basico(100) == 5050
@@ -23,19 +18,14 @@ assert sumatoria_basico(100) == 5050
 
 ###############################################################################
 
-
-def sumatoria_sum(n: int) -> int:
-    suma = list(range(1, n, 1))
-    return sum(suma)
-
-
-n = int(input("Ingresar un número: "))
-print(sumatoria_sum)
-
 """Re-Escribir utilizando la función sum.
     Restricción: No utilizar bucles (FOR, WHILE, etc)
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
+
+def sumatoria_sum(n: int) -> int:
+    return sum(range(1+n))
+
 # NO MODIFICAR - INICIO
 assert sumatoria_sum(1) == 1
 assert sumatoria_sum(100) == 5050
@@ -44,17 +34,21 @@ assert sumatoria_sum(100) == 5050
 
 ###############################################################################
 
-
-from typing import Iterable  # noqa: E402
-
-
+"""Toma un lista de números y devuelve el producto de todos los númereos. Si
+    la lista está vacia debe devolver 0.
+    Restricciones:
+        - No usar bibliotecas auxiliares (Numpy, math, pandas).
+        - Utilizar un bucle FOR
+        - Utilizar múltiples Return
+        - No utilizar ELSE
+    """
 def multiplicar_basico(numeros: Iterable[float]) -> float:
     producto = 1
-    if not(numeros):
+    if numeros=[]:
         return 0
 
-    for i in range(numeros[0], numeros[-1]+1):
-        producto = producto * i
+    for i in range(numeros):
+        producto = producto * (i+1)
     return producto
     
 
@@ -63,16 +57,6 @@ numeros = [5, 6, 7, 8, 9]
 
 print(multiplicar_basico(numeros))
 print(multiplicar_basico([]))
-
-    """Toma un lista de números y devuelve el producto de todos los númereos. Si
-    la lista está vacia debe devolver 0.
-    Restricciones:
-        - No usar bibliotecas auxiliares (Numpy, math, pandas).
-        - Utilizar un bucle FOR
-        - Utilizar múltiples Return
-        - No utilizar ELSE
-    """
-
 # NO MODIFICAR - INICIO
 assert multiplicar_basico([1, 2, 3, 4]) == 24
 assert multiplicar_basico([2, 5]) == 10
