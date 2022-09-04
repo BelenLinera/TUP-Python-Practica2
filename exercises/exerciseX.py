55ef9842a33bfa582a6dd63be73fe76723bf4c6f
amentha <amentha2908@gmail.com>
2022-09-04 18:45:57 -0300

    10 terminado

--- exercises/exerciseX.py
+++ exercises/exerciseX.py
@@ -4,14 +4,19 @@ from typing import List, Union
 
 
 def maximo_recursivo(*args) -> float:
-    """Toma una cantidad arbitraria de números y devuelve el mayor.
+    
+    
 
+
+    
+    """Toma una cantidad arbitraria de números y devuelve el mayor.
     Restricciónes:
         - No utilizar la función max
         - No utilizar la ninguna otra función salvo maximo_recursivo
         - Resolver de manera recursiva
     """
-
+lista = [1, 10, 5, -5]
+print(maximo_recursivo(lista))
 
 # NO MODIFICAR - INICIO
 if __name__ == "__main__":
@@ -29,12 +34,21 @@ from functools import reduce  # noqa: E402
 
 
 def sumatoria_reduce(n: int) -> int:
-    """Devuelve la suma de los números de 1 a N.
 
+    lista = []
+    for x in range(0, n, 1):
+        lista.append(x)
+        lista_definitiva = reduce(lambda x, y: x + y, lista)
+    return lista_definitiva
+
+
+    """Devuelve la suma de los números de 1 a N.
     Restricción: Utilizar la función reduce.
     Referencia: https://docs.python.org/3/library/functools.html#functools.reduce  # noqa: E501
     """
 
+n = int(input("Ingrese un número: "))
+print(sumatoria_reduce(n))
 
 # NO MODIFICAR - INICIO
 if __name__ == "__main__":
@@ -48,12 +62,10 @@ if __name__ == "__main__":
 
 def numeros_al_final_sorted(lista: List[Union[float, str]]) -> List[Union[float, str]]:  # noqa: E501
     """Re-escribir utilizando la función sorted con una custom key.
-
     Restricciones:
         - No utilizar bucles.
         - No utilizar comprensiones.
         - Utilizar un lambda.
-
     Referencia: https://docs.python.org/3/library/functions.html#sorted
     """
 
